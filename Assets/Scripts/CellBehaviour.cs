fa65cbbe93454bd5c05769801d845d0754bdac67
Victor gonnet <vicy@live.Fr>
2021-05-19 16:25:06 +0200

    trying cellular

---
+++ Assets/Scripts/CellBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public struct Cell
+    {
+        public bool isAlive;
+
+        public Cell(bool isAlive)
+        {
+            this.isAlive = isAlive;
+        }
+    }
+
+    public class CellBehavior : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        private bool isAlive = true;
+
+
+        public bool IsAlive
+        {
+            get => isAlive;
+            set
+            {
+                isAlive = value;
+                UpdateColor(isAlive ? Color.white : Color.black);
+            }
+        }
+
+        public void UpdateColor(Color color)
+        {
+            spriteRenderer.color = color;
+        }
+    }
