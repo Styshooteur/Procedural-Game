24003f5db6d13191d6996018041c85b593c952ac
André <a.silva14@hotmail.com>
2021-06-01 22:13:31 +0200

    Direction follows the mouse

---
+++ Assets/Scripts/FaceMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FaceMouse : MonoBehaviour
+{
+    void Update()
+    {
+        faceMouse();        
+    }
+ 
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = UnityEngine.Camera.main.ScreenToWorldPoint(mousePosition);
+ 
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+        );
+ 
+        transform.up = direction;
+    }
+}
